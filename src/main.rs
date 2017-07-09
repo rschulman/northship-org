342be6ca5930caff12fde369272ba08601dece52
Ross Schulman <ross@rbs.io>
2017-07-09 13:49:08 -0400

    Move to new ruma-client api

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #![feature(conservative_impl_trait)]
 
 extern crate futures;
+extern crate hyper;
 extern crate ruma_client;
 extern crate ruma_client_api;
 extern crate ruma_events;
@@ -14,50 +15,62 @@ use std::convert::TryFrom;
 use std::collections::HashMap;
 
 use futures::Future;
+use hyper::client::Connect;
 use ruma_client::{Client, Error};
-use ruma_client_api::r0::alias::get_alias;
-use ruma_client_api::r0::membership::join_room_by_id;
-use ruma_client_api::r0::send::send_message_event;
-use ruma_client_api::r0::sync::sync_events;
+use ruma_client_api::r0;
+// use ruma_client_api::r0::membership::join_room_by_id;
+// use ruma_client_api::r0::send::send_message_event;
+// use ruma_client_api::r0::sync::sync_events;
 use ruma_events::EventType;
 use ruma_events::room::message::{MessageEventContent, MessageType, TextMessageEventContent};
 use ruma_identifiers::RoomAliasId;
 use tokio_core::reactor::Core;
 use url::Url;
 
-fn run<'a>(conn: &'a Client) -> impl Future<Item = (), Error = ruma_client::Error> + 'a {
+fn run<'a, C: Connect>(conn: &'a Client<C>)
+                       -> impl Future<Item = (), Error = ruma_client::Error> + 'a {
+    use r0::sync::sync_events;
+    use r0::membership::join_room_by_id;
+    use r0::send::send_message_event;
     let mut since_time: Option<String> = None;
     loop {
-        conn.request::<sync_events::Endpoint>(sync_events::Request {
-                filter: None,
-                since: since_time,
-                full_state: None,
-                set_presence: None,
-                timeout: None, // Should probably set a timeout...
-            }).and_then(move |response| {
+        sync_events::call(conn,
+                          sync_events::Request {
+                              filter: None,
+                              since: since_time,
+                              full_state: None,
+                              set_presence: None,
+                              timeout: None, // Should probably set a timeout...
+                          })
+            .and_then(move |response| {
                 since_time = Some(response.next_batch);
                 if response.rooms.invite.len() > 0 {
                     for room in response.rooms.invite.keys() {
-                        conn.request::<join_room_by_id::Endpoint>(join_room_by_id::Request {
-                            room_id: room,
-                            third_party_signed: None,
-                        }).and_then(|response| {
-                            let msg = MessageEventContent::Text(TextMessageEventContent {
-                            body: "Hello, I'm Northship. If you need help, just say `help`.".to_owned(),
-                            msgtype: MessageType::Text,
+                        join_room_by_id::call(conn,
+                                              join_room_by_id::Request {
+                                                  room_id: room,
+                                                  third_party_signed: None,
+                                              })
+                            .and_then(|response| {
+                                let msg = MessageEventContent::Text(TextMessageEventContent {
+                                    body: "Hello, I'm Northship. If you need help, just say \
+                                           `help`."
+                                        .to_owned(),
+                                    msgtype: MessageType::Text,
+                                });
+                                send_message_event::call(conn,
+                                                         send_message_event::Request {
+                                                             room_id: room.clone(),
+                                                             event_type: EventType::RoomMessage,
+                                                             txn_id: "1".to_owned(),
+                                                             data: msg,
+                                                         });
                             });
-                            conn.request::<send_message_event::Endpoint>(send_message_event::Request {
-                                room_id: room.clone(),  
-                                event_type: EventType::RoomMessage,
-                                txn_id: "1".to_owned(),
-                                data: msg,
-                            });
-                        });
                     }
                 }
             });
     }
-    futures::done(())
+    futures::done(Ok(()))
 }
 
 fn main() {
